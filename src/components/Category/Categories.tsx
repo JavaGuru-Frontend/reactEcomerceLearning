7f4033cb92c73e5fa489eeaff6174f55be172606
Y031k <bmbacardi@gmail.com>
2025-08-20 23:20:41 +0300

    use_effect_productClick

--- src/components/Category/Categories.tsx
+++ src/components/Category/Categories.tsx
@@ -1,22 +1,45 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-function Categories() {
+interface CategoriesProps {
+	onCategoryClick: (category: string) => void;
+	title: string;
+}
+
+
+function Categories({onCategoryClick, title}: CategoriesProps) {
 	const [categories, setCategories] = useState([]);
+	const [loading, setLoading] = useState(true);
+
 
 
 	useEffect(() => {
 		fetch('https://dummyjson.com/products/category-list')
 		.then(res => res.json())
-		.then(data) => {
-			setCategories(data);
-			console.log(categories);
-		});
-});
+	.then((data) => {
+		setCategories(data);
+		console.log(categories);
+		setLoading(false);
+	});
+}, []);
+
+if (loading) {	
+	return <div>Loading...</div>;
+}
+
 
   return (
-
-	<span>Hi thyere </span>
-
+	<ul>
+		{title && <h2>{title}</h2>}
+		{categories.map((category: string) => (
+			<li 
+				key={category}
+				onClick={() => onCategoryClick(category)}
+			>
+				{category}
+			</li>
+		))}
+	</ul>
+	
   )
 
 }
--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -1,19 +1,45 @@
+
 import './Product.css';
 import products from '../../data/productData';
 import ProductCard from '../../components/ProductCard';
 import Categories from '../../components/Category/Categories';
+import { useEffect, useState } from 'react';
+
+interface Product {
+	id: number;
+	title: string;
+	description: string;
+	price: string;
+}
 
 function Product() {
+	const[products, setProducts] = useState([]);
+
+	const getProductData = (category: string) => {
+		fetch(`https://dummyjson.com/products/category/${category}`)
+		.then(res => res.json())
+		.then((data) => {
+			setProducts(data.products);
+		}) 
+	}
+	
+	useEffect(() => {
+		getProductData('smartphones'); // Fetch smartphones 
+	}, []);
+
   return (
     <div className="product-page">
       <h1>Products</h1>
-		<Categories />
+		<Categories 
+			title="Categories"
+			onCategoryClick={getProductData}
+		/>
 
       <div className="product-list">
-        {products.map((product) => (
+        {products.map((product: Product) => (
           <ProductCard
             key={product.id}
-            name={product.name}
+            name={product.title}
             description={product.description}
             price={product.price}
           />
